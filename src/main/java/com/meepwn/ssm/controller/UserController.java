678a5a92a35c220416f95be85c2da36b8a16f74f
MeePwn <maybewaityou@gmail.com>
2018-12-02 08:24:13 +0800

    first commit

--- src/main/java/com/meepwn/ssm/controller/UserController.java
+++ src/main/java/com/meepwn/ssm/controller/UserController.java
@@ -4,11 +4,7 @@ import com.meepwn.ssm.common.utils.ResponseUtils;
 import com.meepwn.ssm.pojo.User;
 import com.meepwn.ssm.pojo.response.ResponseModel;
 import com.meepwn.ssm.service.UserService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -16,14 +12,13 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Resource
     private UserService userService;
 
-    @ResponseBody
     @RequestMapping(value = "/selectUser.do", method = RequestMethod.POST)
     public ResponseModel selectUser(@RequestBody Map<String, String> params, HttpServletResponse response) throws IOException {
         String id = params.get("id");
@@ -31,14 +26,12 @@ public class UserController {
         return ResponseUtils.responseModel(response, user, params);
     }
 
-    @ResponseBody
     @RequestMapping(value = "/findAllUsers.do", method = RequestMethod.POST)
     public ResponseModel findAllUsers(HttpServletResponse response) throws IOException {
         List<User> users = userService.findAllUsers();
         return ResponseUtils.responseModel(response, users);
     }
 
-    @ResponseBody
     @RequestMapping(value = "/throwsEx.do", method = RequestMethod.POST)
     public ResponseModel throwsEx(HttpServletResponse response) throws IOException {
         throw new RuntimeException("报错了...");
