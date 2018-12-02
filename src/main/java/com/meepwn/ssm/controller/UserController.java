3adbadc1c0774bc4f2647f091e4a902d9e119ec6
MeePwn <maybewaityou@gmail.com>
2018-12-02 08:37:58 +0800

    first commit

--- src/main/java/com/meepwn/ssm/controller/UserController.java
+++ src/main/java/com/meepwn/ssm/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.meepwn.ssm.controller;
 
 import com.meepwn.ssm.common.utils.ResponseUtils;
+import com.meepwn.ssm.enhance.annotation.method.POST;
 import com.meepwn.ssm.pojo.User;
 import com.meepwn.ssm.pojo.response.ResponseModel;
 import com.meepwn.ssm.service.UserService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
@@ -19,20 +22,20 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @RequestMapping(value = "/selectUser.do", method = RequestMethod.POST)
+    @POST("/selectUser.do")
     public ResponseModel selectUser(@RequestBody Map<String, String> params, HttpServletResponse response) throws IOException {
         String id = params.get("id");
         User user = userService.getUser(Integer.parseInt(id));
         return ResponseUtils.responseModel(response, user, params);
     }
 
-    @RequestMapping(value = "/findAllUsers.do", method = RequestMethod.POST)
+    @POST("/findAllUsers.do")
     public ResponseModel findAllUsers(HttpServletResponse response) throws IOException {
         List<User> users = userService.findAllUsers();
         return ResponseUtils.responseModel(response, users);
     }
 
-    @RequestMapping(value = "/throwsEx.do", method = RequestMethod.POST)
+    @POST("/throwsEx.do")
     public ResponseModel throwsEx(HttpServletResponse response) throws IOException {
         throw new RuntimeException("报错了...");
     }
---
+++ src/main/java/com/meepwn/ssm/enhance/annotation/method/POST.java
@@ -0,0 +1,36 @@
+package com.meepwn.ssm.enhance.annotation.method;
+
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@RequestMapping
+public @interface POST {
+
+    String name() default "";
+
+    @AliasFor("path")
+    String[] value() default {};
+
+    @AliasFor("value")
+    String[] path() default {};
+
+    RequestMethod[] method() default {RequestMethod.POST};
+
+    String[] params() default {};
+
+    String[] headers() default {};
+
+    String[] consumes() default {};
+
+    String[] produces() default {};
+
+}
