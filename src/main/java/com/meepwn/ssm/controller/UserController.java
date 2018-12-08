48cffece44befb3d0c8bb8f9234098ba98822947
MeePwn <maybewaityou@gmail.com>
2018-12-08 21:52:56 +0800

    first commit

--- src/main/java/com/meepwn/ssm/controller/UserController.java
+++ src/main/java/com/meepwn/ssm/controller/UserController.java
@@ -2,7 +2,6 @@ package com.meepwn.ssm.controller;
 
 import com.meepwn.ssm.common.constant.response.ResponseEnum;
 import com.meepwn.ssm.enhance.annotation.advice.ResponseAdvice;
-import com.meepwn.ssm.enhance.annotation.method.POST;
 import com.meepwn.ssm.enhance.exception.ParamsPreparedException;
 import com.meepwn.ssm.enhance.validator.UserRequestValidator;
 import com.meepwn.ssm.entity.dto.ResponseDTO;
@@ -12,10 +11,7 @@ import com.meepwn.ssm.entity.po.User;
 import com.meepwn.ssm.service.UserService;
 import org.springframework.validation.DataBinder;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
@@ -35,14 +31,14 @@ public class UserController {
         binder.setValidator(new UserRequestValidator());
     }
 
-    @POST("/selectUser.do")
+    @PostMapping("/selectUser.do")
     @ResponseAdvice(failure = ResponseEnum.USER_NOT_EXIST)
     public Object selectUser(@Valid @RequestBody UserSelectRequestDTO requestDTO, Errors errors) {
         int id = requestDTO.getId();
         return userService.getUser(id);
     }
 
-    @POST("/updateUser.do")
+    @PostMapping("/updateUser.do")
     @ResponseAdvice(failure = ResponseEnum.USER_UPDATE_FAILURE)
     public Object updateUser(@RequestBody UserUpdateRequestDTO requestDTO) {
         User user = requestDTO.getUser();
@@ -50,13 +46,13 @@ public class UserController {
         return user;
     }
 
-    @POST("/findAllUsers.do")
+    @PostMapping("/findAllUsers.do")
     @ResponseAdvice(failure = ResponseEnum.USER_LIST_IS_EMPTY)
     public Object findAllUsers() {
         return userService.findAllUsers();
     }
 
-    @POST("/throwsEx.do")
+    @PostMapping("/throwsEx.do")
     public ResponseDTO throwsEx() {
         throw new ParamsPreparedException("报错了...");
     }
