6ecfd12d3802c556c8aa24131fcec105ab06419c
MeePwn <maybewaityou@gmail.com>
2018-12-10 16:11:40 +0800

    first commit

--- src/main/java/com/meepwn/ssm/common/constant/Constant.java
+++ src/main/java/com/meepwn/ssm/common/constant/Constant.java
@@ -1,4 +1,8 @@
 package com.meepwn.ssm.common.constant;
 
 public class Constant {
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
 }
--- src/main/java/com/meepwn/ssm/entity/dto/user/UserSelectRequestDTO.java
+++ src/main/java/com/meepwn/ssm/entity/dto/user/UserSelectRequestDTO.java
@@ -1,6 +1,10 @@
 package com.meepwn.ssm.entity.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.meepwn.ssm.common.constant.Constant;
 import lombok.Data;
+
+import java.util.Date;
 
 /**
  * @author MeePwn
@@ -9,5 +13,9 @@ import lombok.Data;
 public class UserSelectRequestDTO {
 
     private Integer id;
+    @JsonFormat(pattern = Constant.DATE_FORMAT)
+    private Date date;
+    @JsonFormat(pattern = Constant.DATE_TIME_FORMAT)
+    private Date dateTime;
 
 }
