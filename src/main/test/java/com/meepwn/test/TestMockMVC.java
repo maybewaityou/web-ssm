cc0850e45ce43ae301224bee7fdf9d0cc3689969
MeePwn <maybewaityou@gmail.com>
2018-12-28 07:24:46 +0800

    first commit

--- src/main/test/java/com/meepwn/test/TestMockMVC.java
+++ src/main/test/java/com/meepwn/test/TestMockMVC.java
@@ -3,32 +3,27 @@ package com.meepwn.test;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-config.xml")
-@WebAppConfiguration
+//@WebAppConfiguration
 public class TestMockMVC {
 
-    @Autowired
-    private WebApplicationContext wac;
-
-    private MockMvc mockMvc;
+//    @Autowired
+//    private WebApplicationContext wac;
+//
+//    private MockMvc mockMvc;
 
     @Before
     public void setup() {
         // 初始化一个MockMVC对象的方式有两种：单独设置、web应用上下文设置
         // 建议使用Web应用上下文设置
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+//        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
     }
 
     @Test
