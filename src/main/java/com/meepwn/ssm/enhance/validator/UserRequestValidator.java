6f562f37e0f69eba405cad367daf19c02c13f8f2
MeePwn <maybewaityou@gmail.com>
2019-01-03 11:14:53 +0800

    first commit

--- src/main/java/com/meepwn/ssm/enhance/validator/UserRequestValidator.java
+++ src/main/java/com/meepwn/ssm/enhance/validator/UserRequestValidator.java
@@ -6,6 +6,8 @@ import com.meepwn.ssm.entity.po.User;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.Objects;
+
 /**
  * @author MeePwn
  */
@@ -23,18 +25,16 @@ public class UserRequestValidator implements Validator {
             UserRequestDTO target = (UserRequestDTO) o;
             User user = target.getUser();
             int id = user.getId();
-            if (id < 0) {
-                errors.rejectValue("id", "", "不能小于 0, 当前 id 为: " + id + " .");
-            }
-//            ValidatorFlow.of(user).validate(User::getId, tmpId -> tmpId < 0, "不能小于 0, 当前 id 为: " + id + " .");
+            ValidatorFlow.of(user)
+                    .validate(User::getId, value -> value >= 0, "id 不能小于 0, 当前 id 为: " + id + " .")
+                    .apply();
         } else if (o instanceof UserPanRequestDTO) {
             UserPanRequestDTO target = (UserPanRequestDTO) o;
             Integer id = target.getId();
-            if (id == null) {
-                errors.rejectValue("id", "", "参数不存在.");
-            } else if (id < 0) {
-                errors.rejectValue("id", "", "不能小于 0, 当前 id 为: " + id + " .");
-            }
+            ValidatorFlow.of(target)
+                    .validate(UserPanRequestDTO::getId, Objects::nonNull, "id 参数不存在")
+                    .validate(UserPanRequestDTO::getId, value -> value >= 0, "id 不能小于 0, 当前 id 为: " + id + " .")
+                    .apply();
         }
     }
 
